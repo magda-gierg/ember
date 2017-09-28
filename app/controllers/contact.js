161903ff573615955eb349d9bff38c720098d673
magda-gierg <mjgiergiczny@gmail.com>
2017-09-29 12:45:02 +1300

    contact controllers

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+emailAddress: '',
+
+  isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+  isDisabled: Ember.computed.not('isValid'),
+
 });
