cd72096c6e1a7ca1f0c2fa70c1b4e30384bf86df
magda-gierg <mjgiergiczny@gmail.com>
2017-09-29 11:57:32 +1300

    added contact form

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,13 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
   emailAddress: '',
-  responseMessage: 'email is sent',
+
   isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
-  isDisabled: Ember.computed.empty('emailAddress'),
+  isDisabled: Ember.computed.not('isValid'),
 
   actions: {
-    sendInvitation() {
+
+    saveInvitation() {
+      alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
+      this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+      this.set('emailAddress', '');
     }
   }
+
 });
