3d68abf111da34836f0f92c5a0f45768850693a6
magda-gierg <mjgiergiczny@gmail.com>
2017-10-05 18:12:55 +1300

    is working

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -2,9 +2,20 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-emailAddress: '',
-
-  isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+  emailAddress: '',
+  isValidEmail: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
   isDisabled: Ember.computed.not('isValid'),
 
+  message: '',
+  isMessageEnoughLong: Ember.computed.gte('message.length', 5),
+  isValid: Ember.computed.and('isValidEmail', 'isMessageEnoughLong'),
+
+  actions: {
+
+    sendMessage() {
+      alert(`Sending your message in progress...  ${this.get('emailAddress', 'message')}`);
+      this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+      this.set('emailAddress', '');
+    }
+  }
 });
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -15,5 +15,4 @@ export default Ember.Controller.extend({
       this.set('emailAddress', '');
     }
   }
-
 });
